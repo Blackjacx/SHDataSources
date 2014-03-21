7cf3a579d9dce8a80aa8841a2ca0d60945b04871
Stefan Herold <stefan.herold@gmail.com>
2014-03-21 15:42:16 +0100

    * integrated global header * protected header now hidden

---
+++ Classes/SHDataSources.h
@@ -0,0 +1,11 @@
+//
+//  SHDataSources.h
+//  SHDataSources
+//
+//  Created by Stefan Herold on 21/03/14.
+//  Copyright (c) 2014 Stefan Herold. All rights reserved.
+//
+
+#import <SHArrayDataSource.h>
+#import <SHMutableArrayDataSource.h>
+#import <SHEmptyDataSource.h>
