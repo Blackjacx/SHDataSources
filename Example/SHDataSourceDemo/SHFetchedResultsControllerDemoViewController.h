e95b243f367b19c671f008d637c1c120cdf76f80
Stefan Herold <stefan.herold@gmail.com>
2014-10-08 17:23:18 +0200

    Added support for NSFetchedResultsController (unfinished, untested)

---
+++ Example/SHDataSourceDemo/SHFetchedResultsControllerDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHFetchedResultsControllerDemoViewController.h
+//  SHDataSourceDemo
+//
+//  Created by Stefan Herold on 08/10/14.
+//  Copyright (c) 2014 Stefan Herold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHFetchedResultsControllerDemoViewController : UIViewController
+
+@end
---
+++ Example/SHDataSourceDemo/SHFetchedResultsControllerDemoViewController.m
@@ -0,0 +1,37 @@
+//
+//  SHFetchedResultsControllerDemoViewController.m
+//  SHDataSourceDemo
+//
+//  Created by Stefan Herold on 08/10/14.
+//  Copyright (c) 2014 Stefan Herold. All rights reserved.
+//
+
+#import "SHFetchedResultsControllerDemoViewController.h"
+
+@interface SHFetchedResultsControllerDemoViewController ()
+
+@end
+
+@implementation SHFetchedResultsControllerDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
